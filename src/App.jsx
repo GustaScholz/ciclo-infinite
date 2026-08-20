37a29b77188a57f3112772c096c5850d19309b00
GustaScholz <gustavo.scholz77@gmail.com>
2026-08-20 11:10:01 -0300

    Prepara rota do painel administrativo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
 import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 import { CartProvider } from "./contexts/CartContext";
+import { ProductProvider } from "./contexts/ProductContext";
 import CartDrawer from "./components/cart/CartDrawer";
 
 function App() {
+  const [route, setRoute] = useState(window.location.hash || "#/");
+
+  useEffect(() => {
+    const handleHashChange = () => setRoute(window.location.hash || "#/");
+    window.addEventListener("hashchange", handleHashChange);
+    return () => window.removeEventListener("hashchange", handleHashChange);
+  }, []);
+
+  if (route === "#/admin") return <Admin />;
+
   return (
-    <CartProvider>
-      <Home />
-      <CartDrawer />
-    </CartProvider>
+    <ProductProvider>
+      <CartProvider>
+        <Home />
+        <CartDrawer />
+      </CartProvider>
+    </ProductProvider>
   );
 }
 
