bd67ce511f9a4d8d396a5d20ea7355f395125e53
GustaScholz <gustavo.scholz77@gmail.com>
2026-08-20 13:26:41 -0300

    Centraliza categorias do catálogo

---
+++ src/data/categories.js
@@ -0,0 +1,12 @@
+export const PRODUCT_CATEGORIES = [
+  "Camisetas",
+  "Moletons",
+  "Calças",
+  "Bermudas",
+  "Tênis",
+  "Bonés",
+  "Acessórios",
+  "Promoções",
+];
+
+export const MENU_CATEGORIES = ["Novidades", ...PRODUCT_CATEGORIES];
